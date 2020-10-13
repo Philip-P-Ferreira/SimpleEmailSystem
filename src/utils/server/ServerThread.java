08d4666a1f3f1032f62ebdc1490080332225497d
Philip Ferreira <philip.p.ferreira@vanderbilt.edu>
2020-10-13 17:04:12 -0500

    fixed formatting again

--- src/utils/server/ServerThread.java
+++ src/utils/server/ServerThread.java
@@ -29,12 +29,13 @@ public class ServerThread implements Runnable {
     boolean threadOn = true;
     try {
       while (threadOn) {
+        System.out.print("\nThread " + threadID + " -> ");
+
         // extract request type
         HashMap<String, String> argMap =
             createProtocolMap(serverStream.read(), PAIR_DELIM, PAIR_SEPARATOR);
 
         // handle based on type
-        System.out.print("\nThread " + threadID + "->");
         switch (argMap.get(COMMAND_KEY)) {
           case LOG_IN:
             userName = argMap.get(USERNAME_KEY);
